1afa2ec69da43921c5957e7f882419837c5f3c3a
Adithi <anuguadithi4040@gmail.com>
2021-05-21 09:54:59 +0000

    adding authentication

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
+// const session = require('express-session');
 const path = require('path');
+var cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate');
+var bodyParser = require('body-parser');
+const ejsMate = require('ejs-mate'); //boilerplates
 const catchAsync = require('./utils/catchAsyn'); //error handling part
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Movie = require('./models/movie');
+const User = require('./models/user');
+
+// for authentication
+const passport = require('passport');
+var LocalStatergy = require('passport-local'); //middle-eare for authentucation
+var passportLocalMongoose = require('passport-local-mongoose');
+
 
 mongoose.connect('mongodb://localhost:27017/truely-tell',{
 	useNewUrlParser : true,
@@ -21,21 +31,119 @@ db.once("open",()=>{
 });
 
 app.engine('ejs',ejsMate) // for templates
-app.set('view engine','ejs'); //no need of explicitly specifyinh .ejs
+app.set('view engine','ejs'); //no need of explicitly specifying .ejs
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cookieParser());
 app.set('views',path.join(__dirname,'views'))//access views folder directly
 
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'));
 
+// const sessionConfig={
+// 	secret: 'secret',
+// 	resave: false,
+// 	saveUninitialized: true,
+// 	cookie:{
+// 		httpOnly: true,
+// 		expires: Date.now()+1000*60*60*24*7,
+// 		maxAge: 1000*60*60*24*7
+// 	}
+// }
 
-app.get('/',(req,res)=>{
+// app.use(session(sessionConfig));
+
+
+app.use(require("express-session")({
+	secret : "nothing",
+	resave: false,
+	saveUninitialized: false
+}));
+
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStatergy(User.authenticate()));
+//getting the user into and out
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+app.get('/',isLoggedIn,(req,res)=>{
 	res.render('home');
 });
+
+// app.get('/fakeUser',async(req,res)=>{
+// 	const user = new User({
+// 		email:'aaa@gmail1.com',
+// 		username:'xyz'
+// 	});
+// 	const newUser = await User.register(user,'advitha');
+// 	res.send(newUser);
+// })
 
 app.get('/movies',catchAsync(async(req,res,next)=>{
 	const movies = await Movie.find({});
 	res.render('movies/index',{movies})
 }));
+
+app.get('/books',catchAsync(async(req,res,next)=>{
+	res.send("Page Yet to design!!!");
+}));
+
+app.get('/register',catchAsync(async(req,res,next)=>{
+	res.render('register');
+}));
+
+
+// app.get("/home",isLoggedIn,function(req,res){
+// 	res.render("home");
+// })
+
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+})
+
+app.post('/register', (req, res) => {
+	// res.send(req.body.username);
+    User.register(new User({ username: req.body.username }), req.body.password, (err, user) => {
+        if (err) {
+            console.log(err)
+            return res.render('register')
+        }
+        passport.authenticate("local")(req, res, () => {
+            res.redirect('/movies')
+        })
+    })
+})
+
+app.post('/login', passport.authenticate("local", {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}), (req, res) => { })
+
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect('/login');
+};
+
+
+
+
+
 app.get('/movies/new',catchAsync(async(req,res,next)=>{
 	res.render('movies/new');
 }));
@@ -73,15 +181,13 @@ app.delete('/movies/:id', catchAsync(async(req,res,next)=>{
 
 app.all('*',(req,res,next)=>{
 	next(new ExpressError('Page not found',404));
-})
+});
 
 app.use((err,req,res,next)=>{
 	// res.send("Error page!!!");
 	const { statusCode=500, message="Something went wrong!!!"} = err;
-
 	res.status(statusCode).render('error',{err});
-	
-})
+});
 
 
 app.listen(3000,()=>{
