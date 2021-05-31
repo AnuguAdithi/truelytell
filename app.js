db95cea9a1c13c40baa205c2d731db05f261a8b8
Adithi <anuguadithi4040@gmail.com>
2021-05-31 03:44:20 +0000

    adding comments

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ const express = require('express');
 const app = express();
 // const session = require('express-session');
 const path = require('path');
-var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser'); // form input 
 const mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser'); // form input
 const ejsMate = require('ejs-mate'); //boilerplates
+
+const Joi = require('joi'); //for server side validations
+
 const catchAsync = require('./utils/catchAsyn'); //error handling part
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Movie = require('./models/movie');
 const User = require('./models/user');
+const Comment = require('./models/comment');
 
 // for authentication
 const passport = require('passport');
@@ -23,6 +27,7 @@ mongoose.connect('mongodb://localhost:27017/truely-tell',{
 	useCreateIndex: true,
 	useUnifiedTopology:true
 });
+mongoose.set('useFindAndModify', false);
 
 const db = mongoose.connection;
 db.on("error",console.error.bind(console,"connection error:"));
@@ -41,6 +46,26 @@ app.set('views',path.join(__dirname,'views'))//access views folder directly
 
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'));
+
+//server side validations middleware
+// const validateMovie = (req,res,next)=>{
+// 	const movieSchema = Joi.object({
+// 		movie : Joi.object({
+// 			name: Joi.string().required(),
+// 			image: Joi.string().required(),
+// 			review: Joi.string().required().min(1),
+// 		}).required()
+// 	})
+// 	const error = movieSchema.validate(req.body);
+// 	if(error){
+// 		const msg = error.details.map(el => el.message).join(',');
+// 		throw new ExpressError(msg,400);
+// 	}
+// 	else{
+// 		next();
+// 	}
+// }
+
 
 // const sessionConfig={
 // 	secret: 'secret',
@@ -62,9 +87,6 @@ app.use(require("express-session")({
 	saveUninitialized: false
 }));
 
-
-
-
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStatergy(User.authenticate()));
@@ -76,6 +98,7 @@ passport.deserializeUser(User.deserializeUser());
 
 app.get('/',isLoggedIn,(req,res)=>{
 	res.render('home');
+	
 });
 
 // app.get('/fakeUser',async(req,res)=>{
@@ -87,10 +110,6 @@ app.get('/',isLoggedIn,(req,res)=>{
 // 	res.send(newUser);
 // })
 
-app.get('/movies',catchAsync(async(req,res,next)=>{
-	const movies = await Movie.find({});
-	res.render('movies/index',{movies})
-}));
 
 app.get('/books',catchAsync(async(req,res,next)=>{
 	res.send("Page Yet to design!!!");
@@ -129,7 +148,7 @@ app.post('/register', (req, res) => {
 
 app.post('/login', passport.authenticate("local", {
     successRedirect: '/',
-    failureRedirect: '/login'
+    failureRedirect: '/register'
 }), (req, res) => { })
 
 
@@ -140,24 +159,41 @@ function isLoggedIn(req, res, next) {
     res.redirect('/login');
 };
 
-
-
-
-
 app.get('/movies/new',catchAsync(async(req,res,next)=>{
 	res.render('movies/new');
 }));
 
+app.get('/movies',catchAsync(async(req,res,next)=>{
+	const movies = await Movie.find({});
+	res.render('movies/index',{movies})
+}));
+
 app.post('/movies',catchAsync(async(req,res,next)=>{
 	
-	if(!req.body.movies) throw new ExpressError('Invalid Movie Review ',400); //for missing data
-	const movie = new Movie(req.body.movie);
+	// if(!req.body.movies) throw new ExpressError('Invalid Movie Review ',400); //for missing data
+	// const movieSchema = Joi.object({
+	// 	movie : Joi.object({
+	// 		name: Joi.string().required(),
+	// 		image: Joi.string().required(),
+	// 		review: Joi.string().required().min(1),
+	// 	}).required()
+	// })
+	// const result = movieSchema.validate(req.body);
+	// console.log(result);
+	
+	console.log(req.body);
+	const movie = new Movie({
+	name : req.body.name,
+	image : req.body.image,
+	review : req.body.review
+	});
 	await movie.save();
-	res.redirect(`/movies/${movie._id}`)
+	res.redirect(`/movies/${movie._id}`);
 }));
 
 app.get('/movies/:id',catchAsync(async(req,res,next)=>{
-	const findMovie = await Movie.findById(req.params.id);
+	const findMovie = await Movie.findById(req.params.id).populate('comments');
+	// console.log(findMovie);
 	res.render('movies/show',{movie:findMovie});
 }));
 
@@ -168,9 +204,18 @@ app.get('/movies/:id/edit',catchAsync(async(req,res,next)=>{
 
 app.put('/movies/:id',catchAsync(async(req,res,next)=>{
 	// res.send("It worked!!");
-	const{id} = req.params;
-	const movie = await Movie.findByIdAndUpdate(id,{...req.body.movie});
-	res.redirect(`/movies/${movie._id}`)
+	// const{id} = req.params.id;
+	// console.log(req.params.id,req.body);
+	
+	const movie = {
+		name : req.body.name,
+		image : req.body.image,
+		review : req.body.review
+	};
+	
+	const Updatedmovie = await Movie.findByIdAndUpdate(req.params.id,movie);
+	// console.log(Updatedmovie);
+	res.redirect(`/movies/${Updatedmovie._id}`);
 }));
 
 app.delete('/movies/:id', catchAsync(async(req,res,next)=>{
@@ -178,6 +223,30 @@ app.delete('/movies/:id', catchAsync(async(req,res,next)=>{
 	await Movie.findByIdAndDelete(req.params.id);
 	res.redirect('/movies');
 }));
+
+app.post('/movies/:id/comments',catchAsync(async(req,res)=>{
+	// res.send("You made it!!");
+	const movie = await Movie.findById(req.params.id);
+	const comment = new Comment({
+		body : req.body.body,
+		rating : req.body.rating
+	});
+	
+	movie.comments.push(comment);
+	// console.log(req.body,comment);
+	await comment.save();
+	await movie.save();
+	// console.log(movie);
+	res.redirect(`/movies/${movie._id}`);
+}))
+
+app.delete('/movies/:id/comments/:commentId',catchAsync(async(req,res)=>{
+	const {id,commentId} = req.params;
+	await Movie.findByIdAndUpdate(id,{$pull: {comments: commentId}});
+	await Comment.findByIdAndDelete(commentId);
+	res.redirect(`/movies/${id}`);
+	// res.send("Delete Me!!");
+}))
 
 app.all('*',(req,res,next)=>{
 	next(new ExpressError('Page not found',404));
@@ -190,6 +259,6 @@ app.use((err,req,res,next)=>{
 });
 
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
 	console.log("Serving on port 3000");
 });
--- models/movie.js
+++ models/movie.js
@@ -1,10 +1,31 @@
 const mongoose = require('mongoose');
+const Comment = require('./comment');
 const Schema = mongoose.Schema;
+
 
 const movieSchema = new Schema({
 	name:String,
 	image:String,
-	review:String
+	review:String,
+	comments:[
+		{
+			type:Schema.Types.ObjectId,
+			ref: 'Comment'
+		}
+	]
+});
+
+movieSchema.post('findOneAndDelete',async function(doc){
+	// console.log("you reached!!")
+	if(doc){
+		await Comment.remove({
+			_id:{
+				$in: doc.comments
+			}
+		})
+	}
 })
+
+
 
 module.exports = mongoose.model('movie',movieSchema);
